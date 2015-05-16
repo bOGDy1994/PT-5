ff6846a879830fb5cd4c0147d96a34b7fc59bf84
bOGDy1994 <bogdy.maxim94@yahoo.com>
2015-05-16 12:10:50 +0300

    add PopulateDictionary facility

---
+++ src/logic/PopulateDictionary.java
@@ -0,0 +1,41 @@
+package logic;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+import java.util.TreeMap;
+import org.json.JSONObject;
+import model.Dictionary;
+
+public class PopulateDictionary extends OperationDecorator {
+
+	public PopulateDictionary(Dictionary inDic) {
+		super(inDic);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public TreeMap<String, String> Operation(String word, String definition) {
+		// TODO Auto-generated method stub
+		String temp;
+		Set<String> names;
+		TreeMap<String, String> InitialList = new TreeMap<String, String>();
+		try{
+			BufferedReader b = new BufferedReader(new FileReader("dicinit.json"));
+			temp = b.readLine();
+			JSONObject obj = new JSONObject(temp);
+			names = obj.keySet();
+			for(String it : names)
+				InitialList.put(it.toString(), obj.getString(it.toString()));
+			dic.setDic(InitialList);	
+		}
+		catch(IOException e)
+		{
+			//System.out.println("ioexc");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
